114d6748d453e39c9a4c5fdd7819c7a2671ac265
조병화 <chobyounghwa@jobyeonghwaui-MacBookAir.local>
2022-10-07 12:38:46 +0900

    20221007

--- boj_study/boj_7785.cpp
+++ boj_study/boj_7785.cpp
@@ -1,30 +1,29 @@
-
-#include <stdio.h>
 #include <iostream>
 #include <vector>
-#include <map>
-#include <algorithm>
-#include <unordered_set>
-#include <math.h>
+#include <set>
 
 using namespace std;
 
 int main()
 {
     int n;
-    string name, action;
-    unordered_set <string> m;
-    
-    cin >> n;
+    set <string> names;
+    string input;
+    string inout;
+    vector <string> answer;
+    cin>>n;
     for(int i = 0; i< n; i++)
     {
-        cin >> name >> action;
-        if (action == "enter" && m.find(name) == m.end())
-            m.insert(name);
-        else if (action == "leave" && m.find(name) != m.end())
-            m.erase(name);
+        cin>>input;
+        cin>>inout;
+        if(inout == "enter")
+            names.insert(input);
+        if(inout == "leave")
+            names.erase(input);
     }
-    vector <string> ans(m.begin(), m.end());
-    sort(ans.begin(), ans.end(), greater<>());
-    for (auto x : ans) cout << x<<'\n';
+    for(auto i : names)
+        answer.push_back(i);
+    for(int i = names.size()-1; i>= 0; i--)
+        cout<<answer[i]<<'\n';
+    return 0;
 }
