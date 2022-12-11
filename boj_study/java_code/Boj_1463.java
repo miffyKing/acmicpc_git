bca70f98feb7198b5656377a88389593949facea
miffyKing <whqudghk28@gmail.com>
2022-12-11 13:04:18 +0900

    "12/11 1463"

---
+++ boj_study/java_code/Boj_1463.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+import static java.lang.Math.min;
+
+public class Boj_1463 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int[] table = new int[1000002];
+        table[1] = 0;
+        table[2] = 1;
+        table[3] = 1;
+        for (int i = 4; i <= X; i++) {
+            List<Integer> comp = new ArrayList<>();
+            if (i % 2 == 0) {
+                comp.add(table[i/2] + 1);
+            }
+            if (i % 3 == 0) {
+                comp.add(table[i/3] + 1);
+            }
+            comp.add(table[i-1] + 1);
+            Collections.sort(comp);
+//            for (int j = 0; j < comp.size(); j++) {
+//                System.out.print(comp.get(j) + " ");
+//            }
+//            System.out.println();
+            table[i] = comp.get(0);
+        }
+//        for (int i = 1; i <= X; i++) {
+            System.out.println(table[X]);
+//        }
+    }
+}
