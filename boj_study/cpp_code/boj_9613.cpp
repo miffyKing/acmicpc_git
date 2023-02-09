7d7f448cfd8c3a79a668aa4f0a95c9ddf8578a49
miffyKing <whqudghk28@gmail.com>
2023-02-09 14:30:52 +0900

    02/09 16400

---
+++ boj_study/cpp_code/boj_9613.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int tc;
+    
+    cin>>tc;
+    for (int idx = 0; idx < tc; idx++)
+    {
+        int tmp;
+        vector<int> n_list;
+        cin>>n;
+        for (int i = 0; i < n; i++)
+        {
+            cin>>tmp;
+            n_list.push_back(tmp);
+        }
+
+    }
+    return 0;
+}
