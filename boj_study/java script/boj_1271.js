a5a2507bce036002d8ff8e2251b752a6ea1670be
miffyKing <whqudghk28@gmail.com>
2023-03-10 13:22:15 +0900

    java script

---
+++ boj_study/java script/boj_1271.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split(' ');
+
+//let input = fs.readFileSync(0, 'utf8').toString().split(' ');
+
+const m = BigInt(parseInt(input[0]));
+const n = BigInt(parseInt(input[1]));
+
+
+console.log((m/n).toString());
+console.log((m%n).toString());
