f3b937d5e0b4e1caacd66d2e7b01ae5c6dbe61bb
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-03-24 20:25:53 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a,b,c;
+  int x,y,z;
+  cin>>a>>b>>c;
+  cin>>x>>y>>z;
+  cout<< x-c << ' ' << y/b << ' ' << z-a;
+  return 0;
+}
