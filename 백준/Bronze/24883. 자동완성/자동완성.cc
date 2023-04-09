7d90cce37b5fce76e3f8028ac1b385f755471944
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-04-09 22:17:01 +0900

    [Bronze IV] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char c;
+  cin>>c;
+  if (c == 'N' || c == 'n')
+  {
+    cout<<"Naver D2\n";
+    return 0;
+  }
+  cout<<"Naver Whale\n";
+  return 0;
+}
