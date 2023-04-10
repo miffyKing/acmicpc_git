b905a025397fe6f36239a6fa27d310d7e81f3f87
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-04-10 20:01:29 +0900

    [Bronze I] Title: I am Groot, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17283. I am Groot/I am Groot.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int l,r;
+  cin>>l>>r;
+  float total = 0;
+  int cnt = 2;
+
+  while(1)
+  {
+    
+    float  newsize = (float)l*(float)((float)r/100);
+    //cout<<"QQQ   :   " << newsize << "\n";
+    l = (int)newsize;
+    if (l <= 5)
+      break;
+    total += cnt*(int)newsize;
+    cnt*=2;
+  }
+  cout<<total<<"\n";
+  return 0;
+
+}
