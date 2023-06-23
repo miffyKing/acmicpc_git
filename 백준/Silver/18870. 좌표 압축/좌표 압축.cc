092def29e60ee9ddf23b89e1fb44e80e131e49c5
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-06-23 11:51:27 +0900

    [Silver II] Title: 좌표 압축, Time: 976 ms, Memory: 12408 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <utility>
+using namespace std;
+
+map<int, int> ranks;
+
+int main()
+{
+  int n, tmp;
+  vector<int> input;
+  vector<int> input2;
+  // vector<pair<int, int >> sorted;
+  vector<int> ans;
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> tmp;
+    input.push_back(tmp);
+    input2.push_back(tmp);
+  }
+  sort(input2.begin(), input2.end());
+  input2.erase(unique(input2.begin(), input2.end()), input2.end());
+  for (int i = 0; i < n; i++)
+  {
+    cout<< lower_bound(input2.begin(), input2.end(), input[i]) - input2.begin() << " ";
+  }
+  return 0;
+}
