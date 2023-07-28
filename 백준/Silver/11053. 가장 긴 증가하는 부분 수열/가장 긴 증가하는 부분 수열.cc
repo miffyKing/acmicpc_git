0aa41d82703c375975c1fb81854addbecc248ec9
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-07-28 10:51:28 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,36 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <utility>
 
 using namespace std;
 
+int arr[1002];
 int dp[1002];
+
 int main()
 {
   int n;
-  vector<int> arr;
+  int maxval = -1;
   cin>>n;
-  arr.push_back(0);
-  for(int i = 0; i < n; i++)
+  for (int i = 0; i< n; i++)
   {
-    int tmp;
-    cin>>tmp;
-    arr.push_back(tmp);
+    cin>>arr[i];
   }
-
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
   {
     dp[i] = 1;
-    for (int j= 1; j<i; j++)
+    for (int j = 0; j < i; j++)
     {
-      if (arr[i] > arr[j])
+      if (arr[i] > arr[j] )
       {
         dp[i] = max(dp[i], dp[j]+1);
       }
     }
+    maxval = max(dp[i], maxval);
   }
-  sort(dp+0, dp+n+1);
-  cout<<dp[n];
+  cout<<maxval<<"\n";
   return 0;
 }
