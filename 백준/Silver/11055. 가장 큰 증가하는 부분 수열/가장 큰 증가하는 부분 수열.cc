dcbad958822efbf21f82a5f126da1a2ed08ffa86
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-07-31 20:35:35 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,39 +1,35 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <math.h>
 #include <utility>
-
 using namespace std;
 
 int dp[1002];
-
 int main()
 {
   int n;
   cin>>n;
-  vector<int>arr;
-  
-  long long int sum = 0;
-  for(int i = 0; i <n; i++)
+
+  int arr[1002];
+  for (int i = 1; i <=n; i++)
   {
-    int tmp;
-    cin>>tmp;
-    arr.push_back(tmp);
+    cin>>arr[i];
   }
-  
-  for (int i = 0; i < n; i++)
+
+  int ans = 0;
+
+  for (int i = 1; i <= n; i++)
   {
     dp[i] = arr[i];
-    for (int j = 0; j < i; j++)
+    for (int j = 1; j < i; j++)
     {
-      if (arr[j] < arr[i] && dp[i] < dp[j]+arr[i])
+      if(arr[j] < arr[i] && dp[i] < dp[j]+arr[i])
       {
         dp[i] = dp[j] + arr[i];
       }
     }
+    ans = max(ans, dp[i]);
   }
-
-  sort(dp+0, dp+n);
-  cout<<dp[n-1];
+  cout<<ans;
   return 0;
 }
