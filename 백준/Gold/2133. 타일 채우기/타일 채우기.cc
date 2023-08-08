99244307095c925b52fa0af28982aa7d46f26a9f
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-08-08 20:45:27 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.cc
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -1,34 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    int n;
-    int array[32];
-    
-    cin>>n;
-    array[1] = 0;
-    array[2] = 3;
-    array[3] = 0;
-    array[4] = 11;
-    
-    if(n%2 == 1)
-    {
-        cout<<0<<'\n';
-        return 0;
-    }
-    for(int i = 6; i <= n; i+=2)
-    {
-        array[i] = array[i-2] * 3;
-        for(int j= 2; j< i-2; j+=2)
-        {
-           // cout<<"!!!\n";
-            array[i] += (array[j]*2);
-        }
-        array[i]+=2;
-    }
-    cout<<array[n]<<'\n';
+  int n;
+  int dp[32];
+
+  cin>>n;
+
+  if(n% 2 == 1)
+  {
+    cout<<0;
     return 0;
+  }
+  dp[0] = 1;
+  dp[1] = 0;
+  dp[2] = 3;
+
+  for (int i = 4; i <=n; i = i+2)
+  {
+    dp[i] = dp[i-2] * 3;
+    for (int j = i- 4; j >= 0; j = j-2)
+    {
+      dp[i] = dp[i] + (dp[j]*2);
+    }
+  }
+  cout<<dp[n];
+  return 0;
 }
