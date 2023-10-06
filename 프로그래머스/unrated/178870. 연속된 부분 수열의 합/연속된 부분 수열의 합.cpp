fcb127cf1b6b8ce089a26db9c030427b68eb5a9e
miffyKing <87696004+miffyKing@users.noreply.github.com>
2023-10-06 16:11:31 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 4.08 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -14,24 +14,18 @@ vector<int> solution(vector<int> sequence, int k) {
     
     ans = make_pair(1000000,1000000);  //길이와 종료지점 pair
     
-    cout<< length;
     
     while (end != length)
     {
-        //cout<< start << " " << end << " " << partial_sum << "\n";
-        
         if (partial_sum == k)
         {
-            //cout<<start << " " << end <<"\n";
             if (end - start < ans.first)
             {
-               // cout<<"!!!!\n";
                 ans.first = end - start;
                 ans.second = start;
             }
             partial_sum -= sequence[start];
             start++;
-            //continue;
         }
         
         if (partial_sum < k)
@@ -43,10 +37,8 @@ vector<int> solution(vector<int> sequence, int k) {
         {
             partial_sum -= sequence[start];
             start++;
-        }
-        
+        }    
     }
-    
     answer.push_back(ans.second);
     answer.push_back(ans.second + ans.first);
     return answer;
