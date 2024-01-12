39f7ee8627deb74465bddb8107fc8ae4bda47866
miffyKing <87696004+miffyKing@users.noreply.github.com>
2024-01-12 17:34:19 +0900

    [Silver I] Title: 그림, Time: 72 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,85 +1,82 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
+#include <math.h>
 #include <queue>
 #include <utility>
 
 using namespace std;
 
+// vector<pair<int,int>> 
+int isvisited[502][502] = {0,}; 
+int arr[502][502];
 int dir_x[4] = {-1, 0, 1, 0};
-int dir_y[4] = {0, -1, 0, 1};
+int dir_y[4] = {0, 1, 0, -1};
 
- int n,m;
-int isvisited[502][502];
-int table[502][502];
-int max_val = -1;
-
-void dfs(int x, int y)
+int n,m;
+int maxSize = -1;
+void dfs(int i, int j)
 {
-  queue< pair<int, int> > q;
-  
-  int cnt = 1;
-  q.push(make_pair(x,y));
-  isvisited[x][y] = 1;
+  queue<pair<int,int> > q;
 
-  while (!q.empty())
+  int cnt = 1;
+  q.push( make_pair(i, j) );
+    
+  isvisited[i][j] = 1;
+
+  while(!q.empty())
   {
-    int cur_x = q.front().first;
-    int cur_y = q.front().second;
+    int curX = q.front().first;
+    int curY = q.front().second;
     q.pop();
 
-    for (int i = 0; i <4; i++)
+    for (int cur =0; cur < 4; cur++)
     {
-      int new_x = cur_x + dir_x[i];
-      int new_y = cur_y + dir_y[i];
-
-      if(new_x < 1 || new_x > n || new_y < 1 || new_y > m) continue;
-      if (isvisited[new_x][new_y] == 0 && table[new_x][new_y] == 1)
+      int newX = curX + dir_x[cur];
+      int newY = curY + dir_y[cur];
+      if (newX < 0 || newX >= n || newY < 0 || newY >= m ) continue;
+      if (isvisited[newX][newY] == 1) continue;
+      if (arr[newX][newY] == 1)
       {
-        isvisited[new_x][new_y] = 1;
-        q.push(make_pair(new_x, new_y));
+        isvisited[newX][newY] = 1;
+        q.push(make_pair(newX, newY));
         cnt++;
       }
     }
-  }
 
-  max_val = max(max_val, cnt);
+  }
+  maxSize = max(maxSize, cnt);
 }
+
 int main()
 {
- 
-
   cin>>n>>m;
-  
-
-  for(int i = 1; i<= n; i++)
+  for (int i = 0; i < n; i++)
   {
-    for (int j  =1; j<= m; j++)
+    for (int j=0; j < m; j++)
     {
-      cin>>table[i][j];
+      cin>>arr[i][j];
     }
   }
-
-  int pic_num = 0;
-  for (int i = 1; i <= n; i++)
+  int cnt = 0;
+  for (int i = 0; i < n; i++)
   {
-    for (int j = 1; j<= m; j++)
+    for (int j = 0; j < m; j++)
     {
-      if (table[i][j] == 1 && isvisited[i][j] == 0)   //방문안했을 때 dfs 실행해야해
+      if (isvisited[i][j] == 0 && arr[i][j] == 1)
       {
-        //cout<<"picture begins here : (" << i << " , " << j << ")\n";
-        pic_num++;
-        dfs(i,j);
+        cnt++;
+        dfs(i, j);
+        
       }
     }
   }
-  if (pic_num == 0)
+  if (cnt == 0)
   {
-    cout<< 0 << "\n" << 0 <<"\n";
-  } else 
-  {
-  cout<<pic_num <<"\n";
-  cout<<max_val<<"\n";
+    cout<< "0" << "\n" << "0";
+    return 0;
+    
   }
+  cout<< cnt << "\n" << maxSize << "\n";
   return 0;
 }
