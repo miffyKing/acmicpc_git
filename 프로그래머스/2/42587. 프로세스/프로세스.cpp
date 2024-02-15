683a02bac6db83b38468f9018a40cab3a604aa0c
miffyKing <87696004+miffyKing@users.noreply.github.com>
2024-02-15 18:08:11 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {    
+    queue<int> q;
+    
+    for (int i = 0; i < priorities.size(); i++)
+    {
+        q.push(priorities[i]);
+    }
+    sort(priorities.begin(), priorities.end(), greater<int>());
+    
+    int answer = 0;
+    
+    while(true){
+        if(q.front() == priorities[0]){
+            answer++;
+            q.pop();
+            priorities.erase(priorities.begin());
+            if(location == 0) return answer;
+            else location--;
+        }
+        else{
+            q.push(q.front());
+            q.pop();
+            location--;
+            if(location < 0) 
+                location = q.size() - 1;
+        }
+    }
+    return answer;
+    
+}
